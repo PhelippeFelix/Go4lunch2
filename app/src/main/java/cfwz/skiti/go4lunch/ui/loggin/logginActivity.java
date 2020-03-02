13c324fa2a62545c6ffc1c743ca4d77919cc8119
Sindrosa <skiti1996@live.fr>
2020-03-02 22:23:45 +0100

    empty loggin activity added

---
+++ app/src/main/java/cfwz/skiti/go4lunch/ui/loggin/logginActivity.java
@@ -0,0 +1,21 @@
+package cfwz.skiti.go4lunch.ui.loggin;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import cfwz.skiti.go4lunch.R;
+
+/**
+ * Created by Skiti on 02/03/2020
+ */
+
+public class logginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_log);
+    }
+}
